931917cc7f65cf84e3ce93cee5613191dd0bc87b
Luis <lahoising78@gmailcom>
2019-04-05 20:31:40 -0400

    Add md bootstrap and navbar

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var express = require("express"),
 mongoose.connect("mongodb://localhost:27017/movie-tasklist", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
+    
     res.render("tasks/index");
 });
 
