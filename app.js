3146a75d99992b58702f41670bb3f6501c7fe827
Luis <lahoising78@gmailcom>
2019-04-05 21:46:02 -0400

    add users

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require("express"),
     app = express(),
     mongoose = require("mongoose"),
+    passport = require("passport"),
     bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
     User = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/movie-tasklist", {useNewUrlParser: true});
@@ -9,25 +12,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/scripts"));
+app.use(require("express-session")({
+    secret: "this is a secret... i like hamilton",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
-    var movies = [
-        {
-            title: "Heathers (1989)",
-            due: new Date(2020, 2, 7),
-            status: "plan to watch"
-        },
-        {
-            title: "Hamilton (2015)",
-            due: new Date(2017, 0, 30),
-            status: "plan to watch"
-        },
-        {
-            title: "Dear Evan Hansen (2017)",
-            due: new Date(2019, 5, 26),
-            status: "plan to watch"
-        }
-    ];
+    var movies = [];
     res.render("tasks/index", {movies, movies});
 });
 
